38bceceef3a2d377093645fc0248746560cf6182
Paul Jacobs <pajacobs@tesla.cs.iupui.edu>
2018-12-02 13:46:22 -0500

    switching to wall upon click working, node class refactored to be correct

--- source/Node.js
+++ source/Node.js
@@ -1,10 +1,17 @@
 function Node(scene, src, width, height) {
-	//for dijstras algorithm
-	var isVisited = false;
-	//is this variable is true, then the "wall" barrier image will be displayed and that node is no longer walkable
-	var isClicked = false;
-	
 	var aNode = new Sprite(scene, src, width, height);
+	
+	//for dijstras algorithm
+	aNode.isVisited = false;
+	//is this variable is true, then the "wall" barrier image will be displayed and that node is no longer walkable
+	aNode.isWall = false;
+	
+	aNode.makeWall = function () {
+		if(aNode.isWall == false) {
+			aNode.isWall = true;
+			aNode.setImage("../images/Wall.jpg");
+		}
+	}
 	
 	return aNode;
 }
